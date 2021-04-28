e149cf4153f05e5160c58b53c6447f7f4f5ac2a4
kenny.thuong <kenny.thuong@itlvn.com>
2021-04-28 10:21:55 +0700

    ++update build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgxCombogridLibraryModule } from 'ngx-combogrid-library';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgxCombogridLibraryModule
+    BrowserModule
   ],
-  providers: [
-  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
